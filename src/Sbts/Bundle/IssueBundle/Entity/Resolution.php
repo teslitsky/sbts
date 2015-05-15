e57d7dd2584e8d2a72ec716b445fbd9c886e7d5f
Dmitry Teslitsky <dmitry@teslitsky.info>
2015-05-15 14:09:48 +0300

    Created Issue related Entity stubs

---
+++ src/Sbts/Bundle/IssueBundle/Entity/Resolution.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Sbts\Bundle\IssueBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Resolution
+ *
+ * @ORM\Table(name="sbts_issue_resolution")
+ * @ORM\Entity
+ */
+class Resolution
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Resolution
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
