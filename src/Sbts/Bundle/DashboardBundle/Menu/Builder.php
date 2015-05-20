663b0db977969f723017983e0591c3f2445eb1e3
Dmitry Teslitsky <dmitry@teslitsky.info>
2015-05-20 11:53:05 +0300

    OOT-569: Create user dropdown menu. Attempt to create user menu builder.

--- src/Sbts/Bundle/DashboardBundle/Menu/Builder.php
+++ src/Sbts/Bundle/DashboardBundle/Menu/Builder.php
@@ -9,16 +9,12 @@ class Builder extends ContainerAware
 {
     public function mainMenu(FactoryInterface $factory, array $options)
     {
-        $menu = $factory->createItem('root', array(
-            'childrenAttributes' => array(
-                'class' => 'nav navbar-nav',
-            ),
-        ));
+        $menu = $factory
+            ->createItem('root')
+            ->setChildrenAttribute('class', 'nav navbar-nav');
 
         $menu->addChild('Projects', array('route' => 'sbts_project_list'));
         $menu->addChild('Issues', array('route' => 'sbts_issue_list'));
-        //$menu->addChild('Profile', array('route' => 'fos_user_profile_show'));
-        $menu->addChild('Logout', array('route' => 'fos_user_security_logout'));
 
         return $menu;
     }
---
+++ src/Sbts/Bundle/UserBundle/Menu/Builder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Sbts\Bundle\UserBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+class Builder extends ContainerAware
+{
+    public function userDropdown(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory
+            ->createItem('root')
+            ->setChildrenAttribute('class', 'nav navbar-nav navbar-right');
+
+        $menu
+            ->addChild('User', array('uri' => '#'))
+            ->setAttribute('class', 'dropdown');
+
+        $menu['User']
+            ->addChild('Profile', array('uri' => '#'))
+            ->setAttribute('class', 'dropdown-toggle')
+            ->setAttribute('data-toggle', 'dropdown');
+
+        $menu['User']->addChild('Logout', array('uri' => '#'));
+
+        return $menu;
+    }
+}
