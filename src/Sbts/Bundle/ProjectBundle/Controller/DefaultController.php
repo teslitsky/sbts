bc13a23c63bb92d40357d29a37abda070e57ed52
Dmitry Teslitsky <dmitry@teslitsky.info>
2015-05-20 13:07:06 +0300

    OOT-577: Update Project bundle routing annotations

--- src/Sbts/Bundle/ProjectBundle/Controller/DefaultController.php
+++ src/Sbts/Bundle/ProjectBundle/Controller/DefaultController.php
@@ -6,17 +6,32 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-/**
- * @Route("/project")
- */
 class DefaultController extends Controller
 {
     /**
-     * @Route("/list", name="sbts_project_list")
+     * @Route("/projects", name="sbts_project_list")
      * @return Response
      */
     public function listAction()
     {
         return $this->render('SbtsProjectBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/project/{code}", name="sbts_project_page")
+     * @return Response
+     */
+    public function indexAction()
+    {
+        return $this->render('SbtsProjectBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/project/create", name="sbts_project_create")
+     * @return Response
+     */
+    public function create()
+    {
+
     }
 }
