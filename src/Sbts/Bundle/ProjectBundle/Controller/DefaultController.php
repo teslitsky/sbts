6e6a2a2242b495f0b1ae9c35554f372d73184902
Dmitry Teslitsky <dmitry@teslitsky.info>
2015-05-27 12:00:34 +0300

    OOT-570: Security voter for project

--- src/Sbts/Bundle/ProjectBundle/Controller/DefaultController.php
+++ src/Sbts/Bundle/ProjectBundle/Controller/DefaultController.php
@@ -6,19 +6,30 @@ use Sbts\Bundle\ProjectBundle\Entity\Project;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/project/create", name="sbts_project_create")
+     * @param Request $request
+     *
+     * @return RedirectResponse|Response
      */
     public function createAction(Request $request)
     {
         $project = new Project();
+
+        if (false === $this->get('security.context')->isGranted('create', $project)) {
+            throw new AccessDeniedException('Unauthorised access!');
+        }
+
         $form = $this->createForm('sbts_project_form', $project);
         $form->handleRequest($request);
+
         if ($form->isValid()) {
             $projectManager = $this->get('sbts.project.project_manager');
             $projectManager->saveProject($project);
@@ -26,44 +37,58 @@ class DefaultController extends Controller
             return $this->redirect(
                 $this->generateUrl(
                     'sbts_project_page',
-                    array(
+                    [
                         'project' => $project->getCode(),
-                    )
+                    ]
                 )
             );
         }
 
         return $this->render(
             'SbtsProjectBundle:Default:form.html.twig',
-            array(
+            [
                 'form' => $form->createView(),
-            )
+            ]
         );
     }
 
     /**
      * @Route("/project/{project}", name="sbts_project_page")
      * @ParamConverter("project", class="SbtsProjectBundle:Project", options={"repository_method" = "findByCode"})
+     *
      * @return Response
      */
-    public function indexAction(Project $project)
+    public function viewAction(Project $project)
     {
+        if (false === $this->get('security.context')->isGranted('view', $project)) {
+            throw new AccessDeniedException('Unauthorised access!');
+        }
+
         return $this->render(
             'SbtsProjectBundle:Default:project.html.twig',
-            array(
+            [
                 'project' => $project,
-            )
+            ]
         );
     }
 
     /**
      * @Route("/project/update/{project}", name="sbts_project_update")
      * @ParamConverter("project", class="SbtsProjectBundle:Project", options={"repository_method" = "findByCode"})
+     * @param Request $request
+     * @param Project $project
+     *
+     * @return RedirectResponse|Response
      */
     public function updateAction(Request $request, Project $project)
     {
+        if (false === $this->get('security.context')->isGranted('edit', $project)) {
+            throw new AccessDeniedException('Unauthorised access!');
+        }
+
         $form = $this->createForm('sbts_project_form', $project);
         $form->handleRequest($request);
+
         if ($form->isValid()) {
             $projectManager = $this->get('sbts.project.project_manager');
             $projectManager->saveProject($project);
@@ -71,20 +96,24 @@ class DefaultController extends Controller
             return $this->redirect(
                 $this->generateUrl(
                     'sbts_project_page',
-                    array(
+                    [
                         'project' => $project->getCode(),
-                    )
+                    ]
                 )
             );
         }
 
-        return $this->render('SbtsProjectBundle:Default:form.html.twig', array(
-            'form' => $form->createView(),
-        ));
+        return $this->render(
+            'SbtsProjectBundle:Default:form.html.twig',
+            [
+                'form' => $form->createView(),
+            ]
+        );
     }
 
     /**
      * @Route("/projects", name="sbts_project_list")
+     *
      * @return Response
      */
     public function listAction()
@@ -94,9 +123,9 @@ class DefaultController extends Controller
 
         return $this->render(
             'SbtsProjectBundle:Default:index.html.twig',
-            array(
+            [
                 'projects' => $projects,
-            )
+            ]
         );
     }
 }
---
+++ src/Sbts/Bundle/ProjectBundle/Security/Authorization/Voter/ProjectVoter.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace Sbts\Bundle\ProjectBundle\Security\Authorization\Voter;
+
+use Sbts\Bundle\ProjectBundle\Entity\Project;
+use Sbts\Bundle\UserBundle\Entity\User;
+use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class ProjectVoter implements VoterInterface
+{
+    const VIEW = 'view';
+    const EDIT = 'edit';
+    const CREATE = 'create';
+
+    /**
+     * @param string $attribute
+     *
+     * @return bool
+     */
+    public function supportsAttribute($attribute)
+    {
+        return in_array($attribute, [
+            self::VIEW,
+            self::EDIT,
+            self::CREATE,
+        ]);
+    }
+
+    /**
+     * @param string|Project $project
+     *
+     * @return bool
+     */
+    public function supportsClass($project)
+    {
+        $class = $project;
+
+        if (!is_string($project)) {
+            $class = get_class($project);
+        }
+
+        $supportedClass = 'Sbts\Bundle\ProjectBundle\Entity\Project';
+
+        return $supportedClass === $class || is_subclass_of($class, $supportedClass);
+    }
+
+    /**
+     * @param TokenInterface $token
+     * @param Project|string $project
+     * @param array          $attributes
+     *
+     * @return int
+     */
+    public function vote(TokenInterface $token, $project, array $attributes)
+    {
+        // check if class of this object is supported by this voter
+        if (!$this->supportsClass($project)) {
+            return VoterInterface::ACCESS_ABSTAIN;
+        }
+
+        // check if the voter is used correct, only allow one attribute
+        // this isn't a requirement, it's just one easy way for you to
+        // design your voter
+        if (1 !== count($attributes)) {
+            throw new \InvalidArgumentException(
+                'Only one attribute is allowed for VIEW or EDIT or CREATE'
+            );
+        }
+
+        // set the attribute to check against
+        $attribute = $attributes[0];
+
+        // check if the given attribute is covered by this voter
+        if (!$this->supportsAttribute($attribute)) {
+            return VoterInterface::ACCESS_ABSTAIN;
+        }
+
+        // get current logged in user
+        $user = $token->getUser();
+
+        // make sure there is a user object (i.e. that the user is logged in)
+        if (!$user instanceof UserInterface) {
+            return VoterInterface::ACCESS_DENIED;
+        }
+
+        switch ($attribute) {
+            case self::VIEW:
+                if ($this->adminCanViewProject($user)) {
+                    return VoterInterface::ACCESS_GRANTED;
+                }
+
+                if ($this->managerCanViewProject($user)) {
+                    return VoterInterface::ACCESS_GRANTED;
+                }
+
+                if ($this->operatorCanViewProject($user, $project)) {
+                    return VoterInterface::ACCESS_GRANTED;
+                }
+
+                break;
+
+            case self::CREATE:
+            case self::EDIT:
+                if ($this->userCanEditProject($user)) {
+                    return VoterInterface::ACCESS_GRANTED;
+                }
+
+                break;
+        }
+
+        return VoterInterface::ACCESS_DENIED;
+    }
+
+    /**
+     * @param User $user
+     *
+     * @return bool
+     */
+    public function adminCanViewProject($user)
+    {
+        return $user->hasRole('ROLE_ADMIN');
+    }
+
+    /**
+     * @param User $user
+     *
+     * @return bool
+     */
+    public function managerCanViewProject($user)
+    {
+        return $user->hasRole('ROLE_MANAGER');
+    }
+
+    /**
+     * @param User    $user
+     * @param Project $project
+     *
+     * @return bool
+     */
+    public function operatorCanViewProject($user, $project)
+    {
+        return $project->getUsers()->contains($user);
+    }
+
+    /**
+     * @param User $user
+     * @return bool
+     */
+    public function userCanEditProject($user)
+    {
+        return $user->hasRole('ROLE_ADMIN') or $user->hasRole('ROLE_MANAGER');
+    }
+}
