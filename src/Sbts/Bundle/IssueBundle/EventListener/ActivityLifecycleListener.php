35a41d7e016ddac578aa832048e437a5fb0fadc7
Dmitry Teslitsky <dmitry@teslitsky.info>
2015-06-03 17:59:33 +0300

    OOT-585 Send emails from local dev instance through Gmail

--- src/Sbts/Bundle/IssueBundle/EventListener/ActivityLifecycleListener.php
+++ src/Sbts/Bundle/IssueBundle/EventListener/ActivityLifecycleListener.php
@@ -4,6 +4,7 @@ namespace Sbts\Bundle\IssueBundle\EventListener;
 
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use Sbts\Bundle\IssueBundle\Entity\Activity;
+use Sbts\Bundle\UserBundle\Entity\User;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class ActivityLifecycleListener
@@ -27,28 +28,33 @@ class ActivityLifecycleListener
     public function postPersist(LifecycleEventArgs $args)
     {
         $event = $args->getEntity();
-        $mailer = $this->container->get('mailer');
 
         if ($event instanceof Activity) {
             $recipient = $event
                 ->getIssue()
                 ->getCollaborators()
                 ->map(function ($user) {
+                    /** @var User $user */
                     return $user->getEmail();
                 })
                 ->getValues();
 
+            $mailer = $this->container->get('mailer');
             $message = \Swift_Message::newInstance()
                 ->setSubject(sprintf('News form Issue %s', $event->getIssue()->getCode()))
-                ->setFrom('notifier@sbts.view.pp.ua')
+                ->setFrom('dmitryteslitsky@gmail.com')
                 ->setTo($recipient)
                 ->setBody(
                     $this
                         ->container
                         ->get('twig')
-                        ->render('SbtsIssueBundle:Default:email.txt.twig', [
-                            'event' => $event,
-                        ])
+                        ->render(
+                            'SbtsIssueBundle:Default:email.txt.twig',
+                            [
+                                'event' => $event,
+                            ]
+                        ),
+                    'text/html'
                 );
 
             $mailer->send($message);
