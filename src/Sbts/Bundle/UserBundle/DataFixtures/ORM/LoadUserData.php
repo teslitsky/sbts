c8c3fcff41105319c869fc98f65d3ff4b4979dc7
Dmitry Teslitsky <dmitry@teslitsky.info>
2015-06-04 17:05:10 +0300

    OOT-601 Update user fixture

--- src/Sbts/Bundle/UserBundle/DataFixtures/ORM/LoadUserData.php
+++ src/Sbts/Bundle/UserBundle/DataFixtures/ORM/LoadUserData.php
@@ -5,9 +5,12 @@ namespace Sbts\Bundle\UserBundle\DataFixtures\ORM;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+use Sbts\Bundle\UserBundle\Entity\User;
 
 class LoadUserData extends AbstractFixture implements
     FixtureInterface,
@@ -32,12 +35,13 @@ class LoadUserData extends AbstractFixture implements
     public function load(ObjectManager $om)
     {
         $userManager = $this->container->get('fos_user.user_manager');
-        
+
+        /** @var User $admin */
         $admin = $userManager->createUser();
         $admin->setUsername('admin');
         $admin->setEmail('admin@localhost.loc');
         $admin->setPlainPassword('admin');
-        $admin->setFullname('Test Admin');
+        $admin->setFullName('Test Admin');
         $admin->setEnabled(true);
         $admin->addRole('ROLE_ADMIN');
         $avatar = new UploadedFile(__DIR__ . '/avatars/admin.jpg', 'admin.jpg');
@@ -45,11 +49,12 @@ class LoadUserData extends AbstractFixture implements
         $om->persist($admin);
         $this->addReference('user-admin', $admin);
 
+        /** @var User $manager */
         $manager = $userManager->createUser();
         $manager->setUsername('manager');
         $manager->setEmail('manager@localhost.loc');
         $manager->setPlainPassword('manager');
-        $manager->setFullname('Test Manager');
+        $manager->setFullName('Test Manager');
         $manager->setEnabled(true);
         $manager->addRole('ROLE_MANAGER');
         $avatar = new UploadedFile(__DIR__ . '/avatars/manager.jpg', 'manager.jpg');
@@ -57,11 +62,12 @@ class LoadUserData extends AbstractFixture implements
         $om->persist($manager);
         $this->addReference('user-manager', $admin);
 
+        /** @var User $user */
         $user = $userManager->createUser();
         $user->setUsername('user');
         $user->setEmail('user@localhost.loc');
         $user->setPlainPassword('user');
-        $user->setFullname('Test User');
+        $user->setFullName('Test User');
         $user->setEnabled(true);
         $user->addRole('ROLE_OPERATOR');
         $avatar = new UploadedFile(__DIR__ . '/avatars/user.jpg', 'user.jpg');
